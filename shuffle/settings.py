7993aa8d07caa641f3bd1faaafe3286a6f2b46d0
Mukewa O. Wekalao <onesmus.mukewa@gmail.com>
2023-10-21 15:04:55 +0300

    chore: adds settings to gitignore

--- shuffle/settings.py
+++ shuffle/settings.py
@@ -11,8 +11,14 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 import os
-
 from pathlib import Path
+
+try:
+   import pymysql
+   pymysql.version_info = (1, 4, 6, 'final', 0)
+   pymysql.install_as_MySQLdb()
+except ImportError:
+   pass
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
