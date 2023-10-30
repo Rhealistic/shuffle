88b1cf7b76df837d0305cf27c896ca40502ea783
Mukewa O. Wekalao <onesmus.mukewa@gmail.com>
2023-10-30 12:37:30 +0300

    chore: move files and clean up

--- shuffle/wordpress/forms.py
+++ shuffle/wordpress/forms.py
@@ -3,13 +3,13 @@ from django import forms
 class ParamsForm(forms.Form):
     title = forms.CharField(max_length=100)
     content = forms.CharField(widget=forms.Textarea())
-    status = forms.CharField(max_length=100)
+    status = forms.CharField(max_length=50)
     tags = forms.CharField(max_length=100, required=False)
 
     media_url = forms.URLField(required=False)
 
-    media_alt_text = forms.CharField(max_length=100, required=False)
-    media_caption = forms.CharField(max_length=100, required=False)
+    media_alt_text = forms.CharField(max_length=200, required=False)
+    media_caption = forms.CharField(max_length=200, required=False)
     media_description = forms.CharField(max_length=200, required=False)
 
     meta_description = forms.CharField(max_length=200)
