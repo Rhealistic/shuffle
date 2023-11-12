8f51c537f5592e9c4d77f3cf2b19c9fb6698106b
Mukewa O. Wekalao <onesmus.mukewa@gmail.com>
2023-11-12 21:05:27 +0300

    chore: sets default values for blank on admin

--- shuffle/artist/models.py
+++ shuffle/artist/models.py
@@ -17,20 +17,20 @@ class Artist(models.Model):
     bio   = models.CharField(max_length=320)
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=15)
-    photo = models.URLField(help_text="Link to online photo")
-    instagram = models.URLField(unique=True)
+    photo = models.URLField(help_text="Link to online photo", blank=True, null=True)
+    instagram = models.URLField(blank=True, null=True)
     country = models.CharField(max_length=3, default="KEN")
     
     artist_id = models.UUIDField(max_length=30, default = uuid.uuid4, editable = False)
-    mailerlite_subscriber_id = models.CharField(max_length=30, null=True)
-    mailerlite_subscriber_group_id = models.CharField(max_length=30, null=True)
+    mailerlite_subscriber_id = models.CharField(max_length=30, null=True, blank=True)
+    mailerlite_subscriber_group_id = models.CharField(max_length=30, null=True, blank=True)
 
     opportunity_status = models.CharField(max_length=15, choices=OPPORTUNITY_STATUSES, default="WAITING")
-    invite_status = models.CharField(max_length=15, choices=INVITE_STATUSES, null=True)
+    invite_status = models.CharField(max_length=15, choices=INVITE_STATUSES, null=True, blank=True)
     performance_count = models.PositiveSmallIntegerField(default=0)
 
-    next_performance = models.DateTimeField(null=True)
-    last_performance = models.DateTimeField(null=True)
+    next_performance = models.DateTimeField(null=True, blank=True)
+    last_performance = models.DateTimeField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True, blank=True)
     updated_at = models.DateTimeField(auto_now=True, blank=True)
 
