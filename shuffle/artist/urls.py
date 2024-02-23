771a7cd2004ef11da7bd348c6d7b95927e496d7b
Mukewa O. Wekalao <onesmus.mukewa@gmail.com>
2024-02-23 16:22:42 +0300

    chore: adds uuid to url

--- shuffle/artist/urls.py
+++ shuffle/artist/urls.py
@@ -4,17 +4,17 @@ from . import views
 
 api = [
     path("v1/artists/", views.artist_list),
-    path("v1/artists/<artist_id>", views.artist_view),
+    path("v1/artists/<uuid:artist_id>", views.artist_view),
     path("v1/opportunity", views.opportunity_list),
-    path("v1/opportunity/<opportunity_id>", views.opportunity_update),
+    path("v1/opportunity/<uuid:opportunity_id>", views.opportunity_update),
     path("v1/subscriber/", views.subscriber_list),
-    path("v1/subscriber/<subscriber_id>", views.subscriber_update)
+    path("v1/subscriber/<uuid:subscriber_id>", views.subscriber_update)
 ]
 
 web = [
     path("", views.home),
-    path("subscribe/<organization_slug>/<concept_slug>/", views.subscribe, name='view-concept'),
-    path("subscribe/<organization_slug>/<concept_slug>/apply/", views.subscribe, name='apply-to-concept')
+    path("subscribe/<str:organization_slug>/<str:concept_slug>/", views.subscribe, name='view-concept'),
+    path("subscribe/<str:organization_slug>/<str:concept_slug>/apply/", views.subscribe, name='apply-to-concept')
 ]
 
 urlpatterns = (api + web)
